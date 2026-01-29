b61c75cab484cba8506994688bd81be723a77350
shravyakulal01 <kulalshravya72@gmail.com>
2026-01-29 20:40:55 +0530

    Add files via upload

---
+++ Shravya/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_console_app
+{
+    public partial class Student
+    {
+
+        public int Id;
+        public string Name;
+        public decimal Marks;
+
+        public static string CollegeName;
+
+        public void displayInfo()
+        {
+            Console.WriteLine("id: " + Id);
+            Console.WriteLine("Name is: " + Name);
+            Console.WriteLine("Mark is " + Marks);
+
+        }
+        public Student()
+        {
+            Id = 0;
+            Name = "Not assigned";
+            Marks = 90;
+        }
+        public Student(int id, string name, decimal marks)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Marks = marks;
+        }
+
+        public void displayInfo1()
+        {
+            Console.WriteLine("id: " + Id);
+            Console.WriteLine("Name is: " + Name);
+            Console.WriteLine("College is " + CollegeName);
+
+        }
+        public static bool IsCollegeValid()
+        {
+            return !string.IsNullOrEmpty(CollegeName);
+        }
+    }
+    public partial class Student
+    {
+        public static void Multiply(int x, int y)
+        {
+            int result = x * y;
+            Console.WriteLine("Multiplication: " + result);
+        }
+
+    }
+
+}
+
