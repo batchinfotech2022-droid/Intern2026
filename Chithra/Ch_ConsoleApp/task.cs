2440200e0cc1dddf0a5c8d47dc9db3cca82c1064
Chithra <harishchithra2004@gmail.com>
2026-02-03 08:46:50 +0530

    added task.cs and user.cs files

--- Chithra/Ch_ConsoleApp/task.cs
+++ Chithra/Ch_ConsoleApp/task.cs
@@ -3,10 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CsharpConsoleChApp
 {
-    internal class task
+    public class task
     {
+        private int _taskid;
+        private int _assignedBy;
+        private int _assignedTo;
+        private string _title;
+        private string _description;
+        private DateTime? _deadline;
+        private string _status;
+        private string _createdBy;
+        private DateTime _createdDate;
+        private string _modifiedBy;
+        private DateTime _modifiedDate;
+        private bool _isDeleted;
+
+        public task()
+        {
+            _taskid = 0;
+            _assignedBy = 0;
+            _assignedTo = 0;
+            _title = string.Empty;
+            _description = string.Empty;
+            _deadline = DateTime.Now;
+            _status = string.Empty;
+            _createdBy = string.Empty;
+            _createdDate = DateTime.Now;
+            _modifiedBy = string.Empty;
+            _modifiedDate = DateTime.Now;
+            _isDeleted = false;
+        }
+
+        public int Taskid
+        {
+            get { return _taskid; }
+            set { _taskid = value; }    
+        }
+        public int AssignedBy
+        {
+            get { return _assignedBy; }
+            set { _assignedBy = value; }
+        }
+        public int AssignedTo
+        {
+            get { return _assignedTo; }
+            set { _assignedTo = value; }
+        }
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public DateTime? Deadline
+        {
+            get { return _deadline; }
+            set { _deadline = value; }
+        }
+        public string Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+        public string CreatedBy
+        {
+            get { return _createdBy; }
+            set { _createdBy = value; }
+        }
+        public DateTime CreatedDate
+        {
+            get { return _createdDate; }
+            set { _createdDate = value; }
+        }
+        public string ModifiedBy
+        {
+            get { return _modifiedBy; }
+            set { _modifiedBy = value; }
+        }
+        public DateTime ModifiedDate
+        {
+            get { return _modifiedDate; }
+            set { _modifiedDate = value; }
+        }
+        public bool IsDeleted
+        {
+            get { return _isDeleted; }
+            set { _isDeleted = value; }
+        }
+        public task(int Taskid, int AssignedBy,int AssignedTo, string Status,string Title, string Description,DateTime Deadline, DateTime CreatedDate, DateTime ModifiedDate ,string CreatedBy, string ModifiedBy, bool IsDeleted)
+        {
+            _taskid=Taskid;
+            _assignedBy=AssignedBy;
+            _assignedTo=AssignedTo;
+            _title=Title;
+            _description=Description;
+            _createdBy=CreatedBy;
+            _createdDate=CreatedDate;
+            _modifiedBy=ModifiedBy;
+            _modifiedDate=ModifiedDate;
+            _status=Status;
+            _deadline=Deadline;
+            _isDeleted=IsDeleted;
+        }
     }
-}
+    } 
+
--- Chithra/Ch_ConsoleApp/user.cs
+++ Chithra/Ch_ConsoleApp/user.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace CsharpConsoleChApp
 {
-    internal class user
+    public class user
     {
+        private int _userid;
+        private string _name;
+        private string _designation;
+        private string _createdBy;
+        private DateTime _createdDate;
+        private string _modifiedBy;
+        private DateTime _modifiedDate;
+        private bool _isDeleted;
+        public user()
+        {
+            _userid = 0;
+            _name = string.Empty;
+            _designation = string.Empty;
+            _createdBy = string.Empty;
+            _createdDate = DateTime.Now;
+            _modifiedBy = string.Empty;
+            _modifiedDate = DateTime.Now;
+            _isDeleted = false;
+        }
+        public user(int Userid, string Name, string Designation, DateTime CreatedDate, DateTime ModifiedDate, string CreatedBy, string ModifiedBy, bool IsDeleted)
+        {
+            _userid = Userid;
+            _name = Name;
+            _designation = Designation;
+            _createdBy = CreatedBy;
+            _createdDate = CreatedDate;
+            _modifiedBy = ModifiedBy;
+            _modifiedDate = ModifiedDate;
+            _isDeleted = IsDeleted;
+        }
+        public int UserId
+        {
+            get { return _userid; } 
+            set { _userid = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Designation
+        { 
+            get { return _designation; }
+            set { _designation = value; }
+        }
+        public string CreatedBy
+        {
+            get { return _createdBy; }
+            set { _createdBy = value; }
+        }
+        public DateTime CreatedDate
+        {
+            get { return _createdDate; }
+            set { _createdDate = value; }
+        }
+        public string ModifiedBy
+        {
+            get { return _modifiedBy; }
+            set { _modifiedBy = value; }
+        }
+        public DateTime ModifiedDate
+        {
+            get { return _modifiedDate; }
+            set { _modifiedDate = value; }
+        }
+        public bool IsDeleted
+        {
+            get { return _isDeleted; }
+            set { _isDeleted = value; }
+        }
+        
     }
 }
