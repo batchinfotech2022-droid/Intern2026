399b2ab945a4b862c299d7838ab4087c2abe19f0
shravya <kulalshravya72@gmail.com>
2026-02-04 17:00:33 +0530

    Added new MVC file

---
+++ Shravya/Sh_mvc/Interns_mvc/Interns_mvc/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Interns_mvc.ViewModels;
+
+
+namespace Interns_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            LoginViewModel modal = new LoginViewModel();
+            return View(modal);
+
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel Model)
+        {
+            LoginViewModel model = new LoginViewModel(Model.password, Model.username);
+            model.username = "Sam";
+            ViewBag.Test = "Test";
+            return View(model);
+        }
+
+
+    }
+}
